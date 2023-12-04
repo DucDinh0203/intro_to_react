b26488625ecf2b111d92ae55d81b642f0d2fcf0f
Duc Dinh <fxdinhtriduc@gmail.com>
2023-12-04 15:12:30 +1100

    creating Pet.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+import Pet from "./Pet";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <header>
-                <Link to="/"> Adopt Me! </Link>
-            </header>
-            <Routes>
-                <Route path="/details/:id" element={<Details/>}/>
-                <Route path="/" element={<SearchParams/>}/>
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Luna" animal="dog" breed="Havanese" />
+            <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+            <Pet name="Doink" animal="cat" breed="Mix" />
+        </div>
     );
 };
 
