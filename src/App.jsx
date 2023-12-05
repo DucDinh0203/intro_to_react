cdf28052845645c5e786c09df195dfeff22bf7f4
Duc Dinh <fxdinhtriduc@gmail.com>
2023-12-05 17:20:39 +1100

    refresh the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { createRoot } from "react-dom/client";
-// import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Adopt Me!</h1>
-            <SearchParams/>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
